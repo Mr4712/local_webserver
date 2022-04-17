10084aa9e6546ffb2dbf2c88e7542e4a2bd65051
Torge Bjørner Schinz <68809128+Torte82@users.noreply.github.com>
2022-04-18 00:08:16 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -7,6 +7,6 @@ os.chdir('.')
 server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
 server_object.serve_forever()
 
-address = ("0.0.0.0", 21)  # listen on every IP on my machine on port 21
+address = ("0.0.0.0", 21)
 server = servers.FTPServer(address, FTPHandler)
 server.serve_forever()
