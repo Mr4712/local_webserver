d69f12f41ce8fd49d415dc5f946f15a876ec3378
Torge Bjørner Schinz <68809128+Torte82@users.noreply.github.com>
2022-04-18 00:20:20 +0200

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,12 +1,5 @@
 import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
-from pyftpdlib import servers
-from pyftpdlib.handlers import FTPHandler
-
 os.chdir('.')
 server_object = HTTPServer(server_address=('', 80), RequestHandlerClass=CGIHTTPRequestHandler)
 server_object.serve_forever()
-
-address = ("0.0.0.0", 21)
-server = servers.FTPServer(address, FTPHandler)
-server.serve_forever()
